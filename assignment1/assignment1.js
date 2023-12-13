279b34e6e7f97969e3bce0d5f8c1310689935ae3
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-13 19:56:10 +0200

    Created a function to modify data
    1st try at modifying data, prints out current members

--- assignment1/assignment1.js
+++ assignment1/assignment1.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var data_1 = require("./data");
-console.log(data_1.band);
-console.log(data_1.band.members.current[0]);
-console.log(data_1.band.members.past[data_1.band.members.past.length - 1]);
-var allMembers = data_1.band.members.current.concat(data_1.band.members.past);
+/*
+console.log(band)
+console.log(band.members.current[0])
+console.log(band.members.past[band.members.past.length - 1])
+
+let allMembers = band.members.current.concat(band.members.past)
 for (var i = 0; i < allMembers.length; i++) {
-    console.log(allMembers[i].name.toLowerCase());
+    console.log(allMembers[i].name.toLowerCase())
 }
+*/
+function modifyData(band) {
+    var currentMembers = band.members.current.map(function (member) {
+        return member.name;
+    });
+    var expected = {
+        members: {
+            current: currentMembers
+        }
+    };
+    return expected;
+}
+var output = modifyData(data_1.band);
+console.log(output);
--- assignment1/assignment1.ts
+++ assignment1/assignment1.ts
@@ -1,5 +1,6 @@
 import { band } from "./data"
 
+/*
 console.log(band)
 console.log(band.members.current[0])
 console.log(band.members.past[band.members.past.length - 1])
@@ -8,4 +9,21 @@ let allMembers = band.members.current.concat(band.members.past)
 for (var i = 0; i < allMembers.length; i++) {
     console.log(allMembers[i].name.toLowerCase())
 }
+*/
 
+function modifyData(band) {
+    const currentMembers = band.members.current.map((member) => {
+        return member.name
+    })
+
+    const expected = {
+        members: {
+            current: currentMembers
+        }
+    }
+
+    return expected;
+}
+
+const output = modifyData(band)
+console.log(output)
