6925a85a7402f254e9e2070c517588bbee8f1d41
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-18 20:15:39 +0200

    Tested app functionality
    - Replaced template in app.component.ts with hello world
    - Ran the build with ng serve

--- assignment2/src/app/app.component.ts
+++ assignment2/src/app/app.component.ts
@@ -6,8 +6,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'assignment2';
