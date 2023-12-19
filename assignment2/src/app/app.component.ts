fa69d6b2589f9382ca1ce760b8316c0237cae948
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-19 15:52:22 +0200

    Created simple dialog
    The dialog opens by pressing a button and closes by clicking the X button on the dialog

--- assignment2/src/app/app.component.ts
+++ assignment2/src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DialogComponent],
   //templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  template: `<h1>Hello world!</h1>`,
+  template: `
+    <h1>Hello world!</h1>
+    <button (click)="openDialog()">Open dialog</button>
+    <app-dialog *ngIf="isDialogOpen" [title]="dialogTitle" (closeDialog)="closeDialog()">
+      <p></p>
+    </app-dialog>
+  `,
 })
 export class AppComponent {
   title = 'assignment2';
+  dialogTitle: string = 'Re-usable dialog'
+  dialogContent: string = 'Content'
+  isDialogOpen: boolean = false
+
+  openDialog(): void {
+    this.isDialogOpen = true
+  }
+
+  closeDialog(): void {
+    this.isDialogOpen = false
+  }
 }
--- assignment2/src/app/dialog/dialog.component.ts
+++ assignment2/src/app/dialog/dialog.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.css'
+  styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent {
+  @Input() title!: string
+  @Input() content!: string
+  @Output() closeDialog = new EventEmitter<void>()
 
+  onClose():void {
+    this.closeDialog.emit()
+  }
 }
