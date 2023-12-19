2541e8a1a94b901a3369e0ee71618c09441ea08e
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-19 16:08:02 +0200

    Tested dialog component with form

--- assignment2/src/app/app.component.ts
+++ assignment2/src/app/app.component.ts
@@ -2,18 +2,29 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DialogComponent } from './dialog/dialog.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DialogComponent],
+  imports: [CommonModule, RouterOutlet, DialogComponent, FormsModule],
   //templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   template: `
     <h1>Hello world!</h1>
     <button (click)="openDialog()">Open dialog</button>
     <app-dialog *ngIf="isDialogOpen" [title]="dialogTitle" (closeDialog)="closeDialog()">
-      <p></p>
+      <form #myForm="ngForm" (ngSubmit)="onSubmit(myForm)">
+        <label>
+          Name:
+          <input type="text" name="name" ngModel>
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" ngModel>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </app-dialog>
   `,
 })
@@ -29,5 +40,9 @@ export class AppComponent {
 
   closeDialog(): void {
     this.isDialogOpen = false
+  }
+
+  onSubmit(form: any): void {
+    console.log("Name: " + form.value.name + ", Email: " + form.value.email)
   }
 }
